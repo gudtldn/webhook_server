862fd1a036dad3f16d7cfbb1b6ace096eddf8d90
gudtldn <gudtldn1234@gmail.com>
2024-02-29 17:16:45 +0900

    코드 변경 사항을 설명하는 간결한 커밋 메시지입니다.***
    - 디렉토리 변경 오류 처리
    - 컨테이너 재시작 오류 처리
    - GitHub에서 코드 풀링 오류 처리

--- src/commands.rs
+++ src/commands.rs
@@ -1,24 +1,35 @@
-use log::info;
+use log::{error, info};
 use std::{env, io::Error, process::Command};
 
 pub fn change_directory(path: &str) -> Result<(), Error> {
-    env::set_current_dir(path)?;
+    dbg!(path);
+    if let Err(err) = env::set_current_dir(path) {
+        error!("Failed to change directory: {}", err);
+        return Err(err.into());
+    }
     info!("Changed directory to {}", path);
     Ok(())
 }
 
 pub fn restart_container(container_name: &str) -> Result<(), Error> {
     info!("Restarting container {}", container_name);
-    Command::new("docker")
+    if let Err(err) = Command::new("docker")
         .args(&["restart", container_name])
-        .output()?;
+        .output()
+    {
+        error!("Failed to restart container {}: {}", container_name, err);
+        return Err(err.into());
+    }
     info!("Restarted container {}", container_name);
     Ok(())
 }
 
 pub fn pull_from_github() -> Result<(), Error> {
     info!("Pulling from GitHub");
-    Command::new("git").args(&["pull"]).output()?;
+    if let Err(err) = Command::new("git").args(&["pull"]).output() {
+        error!("Failed to pull from GitHub: {}", err);
+        return Err(err.into());
+    }
     info!("Pulled from GitHub");
     Ok(())
 }
